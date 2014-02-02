27472241ed1f9b955d51a57b521bd0d1124c3f56
Bhagyashree Shekhawat <bbhati@bbhati-ltm1.hsd1.ca.comcast.net>
2014-02-01 19:17:37 -0800

    Initial Commit

---
+++ Twitter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Bhagyashree Shekhawat on 2/1/14.
+//  Copyright (c) 2014 Bhagyashree Shekhawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Twitter
+//
+//  Created by Bhagyashree Shekhawat on 2/1/14.
+//  Copyright (c) 2014 Bhagyashree Shekhawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
